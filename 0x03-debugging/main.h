cfcb6c1c01a5b49e5617ec852ff5dc691ad06c6a
Naglaarahma <113629630+Naglaarahma@users.noreply.github.com>
2022-10-21 01:39:01 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -9,3 +8,4 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
+
