778a97f1cb89bc36878a1711a6bab927dde221da
Naglaarahma <naglaa-atif@outlook.com>
2022-10-22 13:14:51 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,22 @@
-#include "main.h" 
-  
- /** 
-  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase. 
-  */ 
- void print_alphabet_x10(void) 
- { 
-         int count = 0; 
-         char letter; 
-  
-         while (count++ <= 9) 
-         { 
-                 for (letter = 'a'; letter <= 'z'; letter++) 
-                         _putchar(letter); 
-                 _putchar('\n'); 
-         } 
- }
+#include "holberton.h"
+/**
+ *  * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *   *
+ *    * Return: x10 a-z
+ *     */
+void print_alphabet_x10(void)
+{
+		int n, co;
+
+			co = 0;
+
+				while (co < 10)
+						{
+									for (n = 'a'; n <= 'z'; n++)
+												{
+																_putchar(n);
+																		}
+											co++;
+													_putchar('\n');
+														}
+}
