b887cbeffb512a5c955fe03255cd00430a095fd8
Naglaarahma <naglaa-atif@outlook.com>
2022-10-24 15:01:17 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/** reset_to_98 - changes pinter to point to value 98
+ *
+ * @n: pointer to n
+ *
+ * Return: none
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+
+
+
+#endif
