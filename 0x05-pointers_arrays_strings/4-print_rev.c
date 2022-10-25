ec4e2569c277211f783226fe501acf5585e7866b
Naglaarahma <naglaa-atif@outlook.com>
2022-10-24 17:43:02 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in stdout in reverse
+ * @s: string to point 
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	
+		_putchar('\n');
+}
