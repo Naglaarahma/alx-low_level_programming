0a295958349a1efd71c9fe2796fe8706ec91edeb
Naglaarahma <naglaa-atif@outlook.com>
2022-10-27 15:56:13 -0700

    Task 6

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,10 +15,10 @@ char *_strncpy(char *dest, char *src, int n)
 	while (i < n && *(src + i))
 	{
 	     *(dest + i) = *(src + i);
-             i++;
+ 	     i++;
 	}
 	while (i < n)
-        {
+ 	{
 	*(dest + i) = '\0';
           i++;
 	}
