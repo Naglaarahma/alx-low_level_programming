933cae29fdba24743d1187784580f257ba97cca0
Naglaarahma <naglaa-atif@outlook.com>
2022-10-27 14:51:23 -0700

    Task 6

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,15 @@
  * Return: the pointer to dest
 */
 char *string_toupper(char *s)
-	char *string_toupper(char *s)
+
 {
 	int count = 0;
 
 	while (*(s + count) != '\0')
 	{
 		if ((*(s + count) >= 97) && (*(s + count) <= 122))
-		*(s + count) = *(s + count) - 32
-		count++;;
+		*(s + count) = *(s + count) - 32;
+		count++;
 	}
 
 	return (s);
