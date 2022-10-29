7b2dc0bd54f8c123a61b0a4690ac46952e49e758
Naglaarahma <naglaa-atif@outlook.com>
2022-10-29 04:36:21 -0700

    Task 6

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
-#include "main.h"
-#include <stdio.h>
+#i#include <stdio.h>
 /**
  * infinite_add - adds two numbers
  *  * @n1: number one.
@@ -40,3 +39,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 					c2--, dr2 = *(n2 + c2) - 48;
 				else
 					dr2 = 0;
+				bg--, size_r--;
+					}
+	                  if (*(r) == '0')
+			return (r + 1);
+			else
+				return (r);
+}
