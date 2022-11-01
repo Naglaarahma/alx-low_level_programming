a38314edeee93e9f78f9a3bb21cde26b795a2c72
Naglaarahma <naglaa-atif@outlook.com>
2022-11-01 09:03:30 -0700

    Task 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
@@ -11,3 +12,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
+#endif
