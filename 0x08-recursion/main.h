c8cdf6ddc85cafcac1e203c34f3bd25b8a9e6503
Naglaarahma <naglaa-atif@outlook.com>
2022-11-02 10:26:11 -0700

    Task 8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,3 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
+#endif
