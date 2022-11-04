cb331fa30cb4dec65b59aa6b1ac9b9daa067471c
Naglaarahma <naglaa-atif@outlook.com>
2022-11-04 14:41:19 -0700

    Task 8

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,12 @@
  * @s: string
  * Return: no return.
 */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	    if (*s != '\0')
 	    {
 		    _putchar(*s);
-		    _puts_recursion(s + 1)
+		    _puts_recursion(s + 1);
 	    }
 	    else
 		    _putchar('\n');
