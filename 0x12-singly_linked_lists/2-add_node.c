d7fcf6c82c89a058e69f9c09429bfa4623076d56
Naglaarahma <113629630+Naglaarahma@users.noreply.github.com>
2022-11-25 00:08:40 +0200

    Update 2-add_node.c
    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,44 +1,45 @@
-#include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * _strlen - finds the length of a string
- *  @str: string to find the length of
- *
- *  Return: length of string
-*/
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-	return (i);
-}
-/**
- * add_node - adds a node to the beginning of a linked list
- * @head: double pointer to a lined list
- * @str: string to add to the new node
- *
- * Return: pointer to the new node
-*/
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new;
-
-	if (str == NULL)
-		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(new->str);
-	new->next = *head;
-	*head = new;
-	return (new);
-}
+/* 
+  * File: 2-add_node.c 
+  * Auth: Brennan D Baraban 
+  */ 
+  
+ #include "lists.h" 
+ #include <string.h> 
+  
+ /** 
+  * add_node - Adds a new node at the beginning 
+  *            of a list_t list. 
+  * @head: A pointer to the head of the list_t list. 
+  * @str: The string to be added to the list_t list. 
+  * 
+  * Return: If the function fails - NULL. 
+  *         Otherwise - the address of the new element. 
+  */ 
+ list_t *add_node(list_t **head, const char *str) 
+ { 
+         char *dup; 
+         int len; 
+         list_t *new; 
+  
+         new = malloc(sizeof(list_t)); 
+         if (new == NULL) 
+                 return (NULL); 
+  
+         dup = strdup(str); 
+         if (dup == NULL) 
+         { 
+                 free(new); 
+                 return (NULL); 
+         } 
+  
+         for (len = 0; str[len];) 
+                 len++; 
+  
+         new->str = dup; 
+         new->len = len; 
+         new->next = *head; 
+  
+         *head = new; 
+  
+         return (new); 
+ }
